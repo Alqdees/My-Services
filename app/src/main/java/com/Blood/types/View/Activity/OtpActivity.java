956f22b640eb46bdf2d837ba30e1f482cefc2388
Ahmed Shaker <amhmeed31@gmail.com>
2023-05-10 14:55:04 +0300

    add otp in professions actvity

--- app/src/main/java/com/Blood/types/View/Activity/OtpActivity.java
+++ app/src/main/java/com/Blood/types/View/Activity/OtpActivity.java
@@ -7,6 +7,7 @@ import androidx.constraintlayout.utils.widget.MotionButton;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -21,19 +22,24 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 import java.util.HashMap;
+import java.util.Objects;
 
 public class OtpActivity extends AppCompatActivity {
 
 
     private EditText et1;
     private MotionButton btnsubmit;
-    private  String realNumber,verificationId,name,location,type,number;
+    private  String realNumber,verificationId,name,location,type,number,profession;
     private  ProgressBar progressBar;
     private FirebaseAuth mAuth;
     private ActionBar actionBar;
-    private boolean isRegister;
+    private boolean isRegister,isProf;
     private FirebaseFirestore db;
+    private HashMap<String,Object> professions;
+    private String Professions = "professions";
     private HashMap<String,Object> users;
 
     /**
@@ -59,6 +65,7 @@ public class OtpActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
         isRegister = getIntent().getBooleanExtra("isRegister",false);
+        isProf = getIntent().getBooleanExtra("prof",false);
         realNumber = getIntent().getStringExtra("realNumber");
 
         verificationId = getIntent().getStringExtra("verificationId");
@@ -66,6 +73,7 @@ public class OtpActivity extends AppCompatActivity {
         name = getIntent().getStringExtra("name");
         number = getIntent().getStringExtra("number");
         location = getIntent().getStringExtra("location");
+        profession = getIntent().getStringExtra("profession");
         type = getIntent().getStringExtra("type");
 
 //        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
@@ -96,8 +104,10 @@ public class OtpActivity extends AppCompatActivity {
                                     progressBar.setVisibility(View.INVISIBLE);
                                     if (isRegister) {
                                         registerUser();
-
-                                    } else {
+                                    }else if (isProf){
+                                        registerProfessions();
+                                    }
+                                    else {
                                         progressBar.setVisibility(View.INVISIBLE);
                                         Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -107,7 +117,6 @@ public class OtpActivity extends AppCompatActivity {
                                         intent.putExtra("number", number);
                                         intent.putExtra("isEditMode", true);
                                         startActivity(intent);
-
                                     }
                                 }
                                 else {
@@ -121,6 +130,47 @@ public class OtpActivity extends AppCompatActivity {
               }
 
         });
+    }
+
+    private void registerProfessions() {
+
+              FirebaseMessaging.getInstance().getToken()
+          .addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+              Log.d("initVariable", Objects.requireNonNull(task.getException().getMessage()));
+              return;
+            }
+            // Get new FCM registration token
+              professions = new HashMap<>();
+              professions.put("name",name);
+              professions.put("number",number);
+              professions.put("nameProfession",profession);
+              professions.put("token",task.getResult());
+
+
+              db.collection(Professions).document(number)
+                  .set(professions).
+                  addOnCompleteListener(task2 -> {
+                      if (task2.isSuccessful()){
+                          Toast.makeText(
+                              OtpActivity.this,
+                              R.string.register_done,
+                              Toast.LENGTH_LONG).show();
+                          startActivity(new Intent(
+                              OtpActivity.this, SelectActivity.class
+                          ));
+                          finish();
+                      }
+                  }).addOnFailureListener(e -> {
+                      // wait some minute
+                      Log.d("EXCEPTIONFire",
+                          e.getMessage());
+                  });
+
+              // TODO: Store the token in your database or send it to your server
+          });
+         // Here to register user professions
+
     }
 
     private void registerUser() {
--- app/src/main/java/com/Blood/types/View/Activity/ProfessionActivity.java
+++ app/src/main/java/com/Blood/types/View/Activity/ProfessionActivity.java
@@ -1,23 +1,45 @@
 package com.Blood.types.View.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.utils.widget.MotionButton;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import com.Blood.types.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.messaging.FirebaseMessaging;
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class ProfessionActivity extends AppCompatActivity {
   private FirebaseFirestore db;
+  private FirebaseAuth mAuth;
   private TextInputEditText nameET,numberET,nameProfession;
   private HashMap<String,Object> professions;
   private String Professions = "professions";
+  private String name,number,profession;
+  private ProgressBar progressBar;
   private MotionButton sendRequest;
   private ActionBar actionBar;
   @Override
@@ -28,58 +50,188 @@ public class ProfessionActivity extends AppCompatActivity {
     setContentView(R.layout.activity_profession);
     initVariable();
     sendRequest.setOnClickListener(View->{
+          name = nameET.getText().toString();
+          number = numberET.getText().toString();
+          profession = nameProfession.getText().toString();
+
+
+          if (TextUtils.isEmpty(name) ||
+              TextUtils.isEmpty(number)
+              || TextUtils.isEmpty(profession) || number.isEmpty())
+          {
+
+            Toast.makeText(ProfessionActivity.this,
+                "أحد الحقول فارغ", Toast.LENGTH_SHORT).show();
+            return;
+
+          }else if (number.length() < 11){
+            Toast.makeText(
+                ProfessionActivity.this,
+                "الرقم قصير", Toast.LENGTH_LONG).show();
+            return;
+          }
+
+          // this is to register user blood donation
+          else {
+            progressBar.setVisibility(android.view.View.VISIBLE);
+            getNumberUser(number);
+          }
+
       // here to get user token
-      FirebaseMessaging.getInstance().getToken()
-          .addOnCompleteListener(task -> {
-            if (!task.isSuccessful()) {
-              Log.d("initVariable", task.getException().getMessage());
-              return;
-            }
-            // Get new FCM registration token
-            String token = task.getResult();
-            sendRequestProfession(token);
-            // TODO: Store the token in your database or send it to your server
-          });
-    });
+//      FirebaseMessaging.getInstance().getToken()
+//          .addOnCompleteListener(task -> {
+//            if (!task.isSuccessful()) {
+//              Log.d("initVariable", task.getException().getMessage());
+//              return;
+//            }
+//            // Get new FCM registration token
+//            String token = task.getResult();
+//            sendRequestProfession(token);
+//            // TODO: Store the token in your database or send it to your server
+//          });
+    }
+    );
   }
+
+
+  private void getNumberUser(String nb) {
+//            DocumentReference docRef = db.collection(s).document();
+//      Log.d("getNumberUser", "getNumberUser: " +s);
+      CollectionReference collectionRef = db.collection(Professions);
+      collectionRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+          if (task.isSuccessful()) {
+            for (QueryDocumentSnapshot document : task.getResult()) {
+
+              if (!Objects.equals(document.getString("number"), nb)) {
+                if (nb.charAt(0) == '0'){
+                String realNumber = nb.substring(1);
+                  sendVerificationCode(realNumber);
+                  break;
+                }
+              }else {
+                Toast.makeText(
+                    ProfessionActivity.this,
+                    "أسمك موجود",
+                    Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                break;
+              }
+            }
+          }else {
+            Toast.makeText(
+                ProfessionActivity.this,
+                R.string.Error_otp,
+                Toast.LENGTH_SHORT).show();
+            progressBar.setVisibility(View.INVISIBLE);
+          }
+        }
+      }).addOnFailureListener(new OnFailureListener() {
+        @Override
+        public void onFailure(@NonNull Exception e) {
+          Log.d("onFailure", e.getMessage());
+          progressBar.setVisibility(View.INVISIBLE);
+//          progressBar.setVisibility(View.INVISIBLE);
+        }
+      });
+
+
+  }
+
+
+  private void sendVerificationCode(String phoneNumber) {
+
+    PhoneAuthOptions options =
+        PhoneAuthOptions.newBuilder(mAuth)
+            .setPhoneNumber("+964"+phoneNumber)         // Phone number to verify
+            .setTimeout(60L, TimeUnit.SECONDS)   // Timeout and unit
+            .setActivity(ProfessionActivity.this)                          // (optional) Activity for callback binding
+            // If no activity is passed, reCAPTCHA verification can not be used.
+            .setCallbacks(mCallbacks)                   // OnVerificationStateChangedCallbacks
+            .build();
+    PhoneAuthProvider.verifyPhoneNumber(options);
+  }
+
+  private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks =
+      new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential credential) {
+//                signInWithPhoneAuthCredential(credential);
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+
+          progressBar.setVisibility(View.INVISIBLE);
+
+          Toast.makeText(
+              ProfessionActivity.this,
+              e.getMessage(),
+              Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onCodeSent(String verificationId,
+                               PhoneAuthProvider.ForceResendingToken
+                                   token)
+        {
+//
+          progressBar.setVisibility(View.INVISIBLE);
+          Intent intent=new Intent(ProfessionActivity.this,OtpActivity.class);
+          intent.putExtra("name",name);
+          intent.putExtra("number",number);
+          intent.putExtra("profession",profession);
+          intent.putExtra("prof",true);
+          intent.putExtra("verificationId",verificationId);
+          startActivity(intent);
+
+        }
+      };
+
   private void initVariable() {
+
+    mAuth = FirebaseAuth.getInstance();
+    mAuth.setLanguageCode("en");
     db = FirebaseFirestore.getInstance();
     nameET = findViewById(R.id.name);
     numberET = findViewById(R.id.number);
     nameProfession = findViewById(R.id.profession);
     sendRequest = findViewById(R.id.addRequest);
+    progressBar =findViewById(R.id.progress);
+    progressBar.setVisibility(View.GONE);
 
   }
 
-  private void sendRequestProfession(String token){
-    String name = nameET.getText().toString();
-    String number = numberET.getText().toString();
-    String Profession = nameProfession.getText().toString();
-
-
-    professions = new HashMap<>();
-    professions.put("name",name);
-    professions.put("number",number);
-    professions.put("nameProfession",Profession);
-    professions.put("token",token);
-
-    db.collection(Professions).document(number)
-        .set(professions).
-        addOnCompleteListener(task -> {
-          if (task.isSuccessful()){
-            Toast.makeText(
-                ProfessionActivity.this,
-                R.string.register_done,
-                Toast.LENGTH_LONG).show();
-            startActivity(new Intent(
-                ProfessionActivity.this, SelectActivity.class
-            ));
-            finish();
-          }
-        }).addOnFailureListener(e -> {
-          // wait some minute
-          Log.d("EXCEPTIONFire",
-              e.getMessage());
-        });
-  }
+//  private void sendRequestProfession(String token){
+//    String name = nameET.getText().toString();
+//    String number = numberET.getText().toString();
+//    String Profession = nameProfession.getText().toString();
+//
+//
+//    professions = new HashMap<>();
+//    professions.put("name",name);
+//    professions.put("number",number);
+//    professions.put("nameProfession",Profession);
+//    professions.put("token",token);
+//
+//    db.collection(Professions).document(number)
+//        .set(professions).
+//        addOnCompleteListener(task -> {
+//          if (task.isSuccessful()){
+//            Toast.makeText(
+//                ProfessionActivity.this,
+//                R.string.register_done,
+//                Toast.LENGTH_LONG).show();
+//            startActivity(new Intent(
+//                ProfessionActivity.this, SelectActivity.class
+//            ));
+//            finish();
+//          }
+//        }).addOnFailureListener(e -> {
+//          // wait some minute
+//          Log.d("EXCEPTIONFire",
+//              e.getMessage());
+//        });
+//  }
 }
