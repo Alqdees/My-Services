2bb27c91268517b46617c3c3292a8f1bd6ea0fc2
Ahmed Shaker <amhmeed31@gmail.com>
2024-04-03 20:30:02 +0300

    add social

--- app/src/main/java/com/Blood/types/View/Activity/RequestDeleteActivity.java
+++ app/src/main/java/com/Blood/types/View/Activity/RequestDeleteActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.constraintlayout.utils.widget.MotionButton;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
 import com.Blood.types.R;
 
 public class RequestDeleteActivity extends AppCompatActivity {
     private ActionBar actionBar;
     private com.google.android.material.textfield.TextInputEditText
-            Et_name,Et_number,Et_des,Et_location;
+            Et_name,Et_number,Et_des;
+    private ImageView tv_whatsapp,tv_telegram,tv_instagram,tv_messenger;
     private MotionButton btn_request;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,43 @@ public class RequestDeleteActivity extends AppCompatActivity {
         getObj();
         //!///////////////
 
+        tv_instagram.setOnClickListener((View) ->{
+            sendInstagram();
+        });
 
+tv_whatsapp.setOnClickListener((View) ->{
+
+});
+tv_messenger.setOnClickListener((View) ->{
+
+});
+
+tv_telegram.setOnClickListener((View) ->{
+
+});
 
 
     }
 
+    private void sendInstagram() {
+
+    }
+
     private void getObj() {
+
+        // initialize
+
         Et_name = findViewById(R.id.nameEt);
         Et_number = findViewById(R.id.numberEt);
-        Et_des= findViewById(R.id.desEt);
-        Et_location = findViewById(R.id.locationEt);
+        Et_des = findViewById(R.id.desEt);
+        // initialize social
         btn_request = findViewById(R.id.registerBtn);
+        tv_instagram = findViewById(R.id.instagram);
+        tv_telegram = findViewById(R.id.telegram);
+        tv_whatsapp = findViewById(R.id.whats_app);
+        tv_messenger = findViewById(R.id.messenger);
+        //
+
 
     }
 }
